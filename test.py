045ac3228acf54e38e4d61d4aed8e7f5b57b97fe
Cynthia Liu <cynliu98@mit.edu>
2020-01-01 01:51:47 -0500

    I hate oauth, permission error on write

--- test.py
+++ test.py
@@ -6,14 +6,15 @@ from dotenv import load_dotenv
 from sheet_connect import *
 
 load_dotenv()
-token = 'NjYxNjU1MDU5MDk1ODc5Njkz.XgulCw.ehY8jPLqbeJvTt86zzjtm0ghc0A'
+token = 'NjYxNjU1MDU5MDk1ODc5Njkz.XgwoBw.jyIxP1fB2rEd1EsKMmN5ynC-WeY'
 
 client = discord.Client()
+service = connect() # connect to google sheets
+spreadsheet_id = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    service = connect()
     print_sheet_contents(service)
 
 @client.event
@@ -26,5 +27,18 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await message.channel.send(msg)
+
+    if message.content.startswith('https://osu.ppy.sh/u'):
+        register(message)
+
+# take a message and send it to sheet
+def register(message):
+    body = {
+        'values': [[str(message.content)]]
+    }
+    print (str(message.content))
+    result = service.spreadsheets().values().append(
+        spreadsheetId=spreadsheet_id, range='Sheet1!A1',
+        valueInputOption="RAW", body=body).execute()
 
 client.run(token)
